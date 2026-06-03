40aa940c5fbffaac02fd2d6a6e85b0d401a4a4ab
nishanafathima-dev <nishanaf731@gmail.com>
2026-06-03 15:04:49 +0530

    function continued

--- main.go
+++ main.go
@@ -18,12 +18,31 @@
 // 	fmt.Println("My favorite number is", rand.Intn(5))
 // }
 
-package main 
-import "fmt"
+// package main 
+// import "fmt"
 
-func add(x int,y int,w int,n int)int{
-    return x+y+w+n
-}
-func main(){
-    fmt.Println(add(1,2,3,4))
-}
+// func add(x int,y int,w int,n int)int{
+//     return x+y+w+n
+// }
+// func main(){
+//     fmt.Println(add(1,2,3,4))
+// }
+// package main 
+// import "fmt"
+// func add(x ,y,w int)int{
+//     return x+y+w
+// }
+// func main(){
+//     fmt.Println(add(12,23,34))
+// }
+
+// package main 
+// import "fmt"
+
+// func swap(x,y,z string)(string,string,string){
+//     return y,x,z
+// }
+// func main(){
+//     a,b,c:=swap("nishana","love","niyas")
+//     fmt.Println(a,b,c)
+// }
