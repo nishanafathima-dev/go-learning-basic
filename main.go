f41133c053898dcd8f82fd6168a6f94c31d857c3
nishanafathima-dev <nishanaf731@gmail.com>
2026-06-04 15:12:22 +0530

    variable declarations

--- main.go
+++ main.go
@@ -46,3 +46,41 @@
 //     a,b,c:=swap("nishana","love","niyas")
 //     fmt.Println(a,b,c)
 // }
+// package main 
+// import "fmt"
+
+// func split(sum int)(x,y int){
+//     x= sum*10/4
+//     y=sum-x
+//     return 
+// }
+// func main(){
+//     fmt.Println(split(17))
+// }
+
+// package main 
+// import "fmt"
+// var c,python,java bool
+// func main(){
+//     var i int
+//     fmt.Println(i,c,python,java)
+// }
+
+// package main 
+// import "fmt"
+
+// func main(){
+//     var i,j =1,2
+//     var c,python,java = true,false,"no!"
+//     fmt.Println(i,j,c,python,java)
+// }
+
+// "ignoreDeprecations": "6.0",
+// package main 
+// import "fmt"
+// var name string = "Nishana" 
+// func main(){
+//     user:= "Nishana"   //:= is used for define type of variable and assign value to it and it can only be used inside a function
+//     fmt.Println("Hello",user)
+//     fmt.Println("Hello",name)
+// }
