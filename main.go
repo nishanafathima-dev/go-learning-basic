5f848659ef7ab954b91d567ada31083ea5097766
nishanafathima-dev <nishanaf731@gmail.com>
2026-06-05 12:06:39 +0530

    go basics

--- main.go
+++ main.go
@@ -84,3 +84,40 @@
 //     fmt.Println("Hello",user)
 //     fmt.Println("Hello",name)
 // }
+// package main 
+// import (
+//     "fmt"
+//     "math/cmplx"
+// )
+// var(
+//     tobe bool = false
+//     maxInt uint64 = 1<<64 -1 //uint64 is an unsigned integer type that can hold values from 0 to 2^64-1
+//     z complex128 = cmplx.Sqrt(-5 + 12i) //complex128 is a complex number type that can hold values with a real part and an imaginary part, each of which is a float64. The cmplx.Sqrt function calculates the square root of a complex number, in this case, -5 + 12i.
+// )
+// func main(){
+//     fmt.Printf("Type:%T Value:%v\n",tobe,tobe)
+//     fmt.Printf("Type:%T Value:%v\n",maxInt,maxInt)
+//     fmt.Printf("Type:%T Value:%v\n",z,z)
+// }
+// int8 //the int8 type is an integer type that can hold values from -128 to 127. It is a signed 8-bit integer, which means it can represent both positive and negative numbers. The int8 type is often used when you need to save memory and you know that the values you will be working with will fit within this range.
+
+// int      → Whole numbers
+// float64  → Decimal numbers
+// string   → Text
+// bool     → true/false
+// byte     → File/binary data
+// rune     → Characters
+
+// package main 
+// import "fmt"
+
+// func main(){
+//     var i int
+//     var f float64
+//     var b bool
+//     var s string
+//     fmt.Printf("Default value of int: %d\n", i)
+//     fmt.Printf("Default value of float64: %f\n", f)
+//     fmt.Printf("Default value of bool: %t\n", b)
+//     fmt.Printf("Default value of string: %q\n", s)
+// }
